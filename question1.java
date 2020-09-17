5c76c6ddcf97a2af07c625666db19533d9943957
Donwebdevelop <68665787+Donwebdevelop@users.noreply.github.com>
2020-09-17 11:43:43 +0000

    Question1

---
+++ question1.java
@@ -0,0 +1,15 @@
+package com.strathjava;
+
+public class question1 {
+    public static void main(String[] args) {
+        // write your code here
+        String[] info = {"Name  : Josh", "Gender: Male"};
+
+        for(int i=0; i<2; i++){
+            System.out.println(info[i]);
+        }
+
+
+
+    }
+}
