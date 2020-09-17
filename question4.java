141d7874203a730024805ffd58406de2b8719cc9
Donwebdevelop <68665787+Donwebdevelop@users.noreply.github.com>
2020-09-17 14:44:37 +0000

    added sum of odd numbers

--- question4.java
+++ question4.java
@@ -6,15 +6,17 @@ import java.util.*;
 
 public class question4 {
     public static void main(String[] args) {
-        int number= 0;
+        int sum= 0;
         List<Integer> numbers  = new ArrayList<Integer>();
         for (int i=1;i<=100;i++){
             if (i%2!=0){
                 numbers.add(i);
+                sum+=i;
             }
 
 
         }
         System.out.println(numbers);
+        System.out.println(sum);
     }
 }
